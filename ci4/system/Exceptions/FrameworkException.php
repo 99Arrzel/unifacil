6432ae648da6e82bd7662236a539a53fb2c638c7
rats4final <80012704+rats4final@users.noreply.github.com>
2021-06-07 03:47:34 -0400

    back

--- ci4/system/Exceptions/FrameworkException.php
+++ ci4/system/Exceptions/FrameworkException.php
@@ -30,7 +30,7 @@ class FrameworkException extends RuntimeException implements ExceptionInterface
 
 	public static function forInvalidFile(string $path)
 	{
-		//return new static(lang('Core.invalidFile', [$path]));
+		return new static(lang('Core.invalidFile', [$path]));
 	}
 
 	public static function forCopyError(string $path)

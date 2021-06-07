537cdd10846fb0cdb3b4b335ed2d4fd500468bd6
CraniumAdamantium <af.carrillo@live.com>
2021-06-07 00:31:19 -0400

    XD

--- ci4/app/Views/admin/usuarios.php
+++ ci4/app/Views/admin/usuarios.php
@@ -7,7 +7,7 @@
 <script>
 var datos = <?= json_encode($usuario)?>;
 const grid = new Grid({
-    columns: [ {id:'NOMBRE', NOMBRE:'NOMBRE'},{id:'apellido', apellido:'APELLIDO'}],
+    
     search: true,
     data:datos,
   }).render(document.getElementById("wrapper"));

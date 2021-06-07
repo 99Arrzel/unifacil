d8d91cacb997323c1c57db5ee75b9532d368ed43
CraniumAdamantium <af.carrillo@live.com>
2021-06-07 04:08:47 -0400

    xd

--- ci4/app/Views/templates/header.php
+++ ci4/app/Views/templates/header.php
@@ -29,8 +29,8 @@
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <!-- Sweet Alert -->
     <!-- GridJS  -->
-    <link href="https://unpkg.com/tabulator-tables@4.9.3/dist/css/tabulator.min.css" rel="stylesheet">
-    <script type="text/javascript" src="https://unpkg.com/tabulator-tables@4.9.3/dist/js/tabulator.min.js"></script>
+    <script src="https://unpkg.com/gridjs/dist/gridjs.production.min.js"></script>
+    <link href="https://unpkg.com/gridjs/dist/theme/mermaid.min.css" rel="stylesheet" />
     <!-- GridJS  -->
     <script>
         function openNav() {

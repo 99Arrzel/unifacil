29a838839f7a05e0deeb9513ab359cac13ce545b
CraniumAdamantium <af.carrillo@live.com>
2021-06-07 11:31:23 -0400

    DatatablesSinVueltaAtrás

--- ci4/app/Views/templates/header.php
+++ ci4/app/Views/templates/header.php
@@ -28,10 +28,16 @@
     <!-- Sweet Alert -->
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <!-- Sweet Alert -->
-    <!-- GridJS  -->
-    <script src="https://unpkg.com/gridjs/dist/gridjs.production.min.js"></script>
-    <link href="https://unpkg.com/gridjs/dist/theme/mermaid.min.css" rel="stylesheet" />
-    <!-- GridJS  -->
+    <!-- DatatablesCDN  -->
+    <link rel="stylesheet" type="text/css"
+        href="https://cdn.datatables.net/v/bs4/jszip-2.5.0/dt-1.10.25/af-2.3.7/b-1.7.1/b-html5-1.7.1/b-print-1.7.1/date-1.1.0/fh-3.1.9/r-2.2.8/sl-1.3.3/datatables.min.css" />
+
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
+    <script type="text/javascript"
+        src="https://cdn.datatables.net/v/bs4/jszip-2.5.0/dt-1.10.25/af-2.3.7/b-1.7.1/b-html5-1.7.1/b-print-1.7.1/date-1.1.0/fh-3.1.9/r-2.2.8/sl-1.3.3/datatables.min.js">
+    </script>
+    <!-- DatatablesCDN  -->
     <script>
         function openNav() {
             document.getElementById("mySidenav").style.width = "250px";

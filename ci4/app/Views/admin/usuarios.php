4f4d77644743c64169fe1c446dc18f9936f17415
CraniumAdamantium <af.carrillo@live.com>
2021-06-07 05:13:24 -0400

    XD

--- ci4/app/Views/admin/usuarios.php
+++ ci4/app/Views/admin/usuarios.php
@@ -114,7 +114,7 @@
             {
                 name: 'Editar',
                 formatter: (cell, row) => html(
-                    `<input id="bttnEditar${row.cells[0].data}" class="btn btn-warning" onclick="editar(${row.cells[0].data})">Editar</input>`
+                    `<button id="bttnEditar${row.cells[0].data}" class="btn btn-warning" onclick="editar(${row.cells[0].data})">Editar</button>`
                 ),
 
                 sort: {

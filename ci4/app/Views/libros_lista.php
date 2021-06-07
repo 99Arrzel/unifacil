ecf95b32b51a9ba19664fbc535b82c7d184fc402
rats4final <80012704+rats4final@users.noreply.github.com>
2021-06-07 01:06:57 -0400

    xd2

--- ci4/app/Views/libros_lista.php
+++ ci4/app/Views/libros_lista.php
@@ -36,8 +36,8 @@
             <h1 class="text-center display-5">LIBROS</h1>
         </div>
         <div class='container' style='margin-top:3em;'>
-        <?php foreach ($libros as $libro): ?>
             <div class='card-columns custom-columns'>
+            <?php foreach ($libros as $libro): ?>
                 <div class='card'>
                     <div class='card-body'>
                      <h5 class='card-title' align='center'><?= $libro->nombreLibro;?></h5>
@@ -51,8 +51,8 @@
                       </ul>
                     </div>
                 </div>
-            </div>
             <?php endforeach; ?>
+            </div>
         </div>                    
 
     <?php else:?>

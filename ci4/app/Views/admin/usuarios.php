0b0c714ece831213124b360c5936e7bcade05cd1
CraniumAdamantium <af.carrillo@live.com>
2021-06-07 00:25:01 -0400

    XD

--- ci4/app/Views/admin/usuarios.php
+++ ci4/app/Views/admin/usuarios.php
@@ -1,7 +1,7 @@
 <div id="wrapper"></div>
 
 <script>
-var datos = <?= json_encode($usuario);
+var datos = <?= json_encode($usuario)?>;
 new gridjs.Grid({
     data:datos,
     columns: [ {data:'NOMBRE'},{data:'APELLIDO'}],

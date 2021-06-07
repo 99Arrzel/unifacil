3a86801e4c5dd582cd6b556d54f4d2cd170bc0f4
CraniumAdamantium <af.carrillo@live.com>
2021-06-07 05:07:03 -0400

    xd

--- ci4/app/Views/admin/usuarios.php
+++ ci4/app/Views/admin/usuarios.php
@@ -106,6 +106,10 @@
                     `<button class="btn btn-info" onclick="sus${row.cells[0].data}"> ${cell}</button>`)
             },
             {
+                id: 'NIVEL',
+                name: 'Nivel',
+            },
+            {
                 name: 'Editar',
                 formatter: (cell, row) => html(
                     `<button id="bttnEditar${row.cells[0].data}" class="btn btn-warning" onclick="editar(${row.cells[0].data})">Editar</button>`

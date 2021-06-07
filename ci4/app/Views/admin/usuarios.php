afbc3bf9313616644292778680cc0caf95740b38
CraniumAdamantium <af.carrillo@live.com>
2021-06-07 18:29:21 -0400

    Language

--- ci4/app/Views/admin/usuarios.php
+++ ci4/app/Views/admin/usuarios.php
@@ -80,40 +80,107 @@ endif;
 </div>
 
 <script type="module">
-var datos = <?= json_encode($usuario)?>;
-var editIcon = function(cell, formatterParams){
-    return "<button class='btn btn-warning'>Editar</button>"
-};
-var downIcon = function(cell, formatterParams){
-    var valor= cell.getValue();
-    return "<button id='"+valor+"' class='btn btn-danger'>Dar de baja</button>"
-};
+    var datos = <?= json_encode($usuario)?> ;
+    var editIcon = function(cell, formatterParams) {
+        return "<button class='btn btn-warning'>Editar</button>"
+    };
+    var downIcon = function(cell, formatterParams) {
+        var valor = cell.getValue();
+        return "<button id='" + valor + "' class='btn btn-danger'>Dar de baja</button>"
+    };
+    var esp =
+        "{\r\n            \"pagination\":{\r\n                \"first\":\"Primero\",\r\n                \"first_title\":\"Primera p\u00E1gina\", \r\n                \"last\":\"\u00DAltima\",\r\n                \"last_title\":\"\u00DAltima p\u00E1gina\",\r\n                \"prev\":\"Previa\",\r\n                \"prev_title\":\"P\u00E1gina previa\",\r\n                \"next\":\"Siguiente\",\r\n                \"next_title\":\"Siguiente p\u00E1gina\",\r\n            },\r\n  }";
 
-var table = new Tabulator("#UsuariosActivos", {
-    data:datos, //assign data to table
-    layout:"fitColumns",      //fit columns to width of table
-    responsiveLayout:"hide",  //hide columns that dont fit on the table
-    tooltips:true,            //show tool tips on cells
-    addRowPos:"top",          //when adding a new row, add it to the top of the table
-    history:true,             //allow undo and redo actions on the table
-    pagination:"local",       //paginate the data
-    paginationSize:7,         //allow 7 rows per page of data
-    movableColumns:true,      //allow column order to be changed
-    //resizableRows:true,       //allow row order to be changed
-    initialSort:[             //set the initial sort order of the data
-        {column:"NOMBRE", dir:"asc"},
-    ],
-    columns:[                 //define the table columns
-        {title:"Nombre", field:"NOMBRE", editor:"input"},
-        {title:"Apellido", field:"APELLIDO", editor:"input"},
-        {title:"Login", field:"LOGIN", width:95, editor:"select", editorParams:{values:["male", "female"]}},
-        {title:"Email", field:"EMAIL", editor:"input"},
-        {title:"Nivel", field:"NIVEL", editor:"select", editorParams:{values:["Admin", "Publisher", "Usuario"]}},
-        {title:"Suscrito", field:"SUSCRITO",  formatter:"tickCross", sorter:"boolean", editor:true},
-        {formatter:editIcon, hozAlign:"center", cellClick:function(e, cell){alert("XD")}},
-        {title:"Eliminar", field: "IDUSER", formatter:downIcon, hozAlign:"center", cellClick:function(e, cell){alert("XD")}},
-    ],
+    var table = new Tabulator("#UsuariosActivos", {
+        locale: true,
+        langs: {
+            "es-ES": {
+                "pagination": {
+                    "first": "Primero",
+                    "first_title": "Primera página",
+                    "last": "Última",
+                    "last_title": "Última página",
+                    "prev": "Previa",
+                    "prev_title": "Página previa",
+                    "next": "Siguiente",
+                    "next_title": "Siguiente página",
+                },
+            }
+        },
+        data: datos, //assign data to table
+        layout: "fitColumns", //fit columns to width of table
+        responsiveLayout: "hide", //hide columns that dont fit on the table
+        tooltips: true, //show tool tips on cells
+        addRowPos: "top", //when adding a new row, add it to the top of the table
+        history: true, //allow undo and redo actions on the table
+        pagination: "local", //paginate the data
+        paginationSize: 7, //allow 7 rows per page of data
+        movableColumns: true, //allow column order to be changed
+        //resizableRows:true,       //allow row order to be changed
+        initialSort: [ //set the initial sort order of the data
+            {
+                column: "NOMBRE",
+                dir: "asc"
+            },
+        ],
+        columns: [ //define the table columns
+            {
+                title: "Nombre",
+                field: "NOMBRE",
+                editor: "input"
+            },
+            {
+                title: "Apellido",
+                field: "APELLIDO",
+                editor: "input"
+            },
+            {
+                title: "Login",
+                field: "LOGIN",
+                width: 95,
+                editor: "select",
+                editorParams: {
+                    values: ["male", "female"]
+                }
+            },
+            {
+                title: "Email",
+                field: "EMAIL",
+                editor: "input"
+            },
+            {
+                title: "Nivel",
+                field: "NIVEL",
+                editor: "select",
+                editorParams: {
+                    values: ["Admin", "Publisher", "Usuario"]
+                }
+            },
+            {
+                title: "Suscrito",
+                field: "SUSCRITO",
+                formatter: "tickCross",
+                sorter: "boolean",
+                editor: true
+            },
+            {
+                formatter: editIcon,
+                hozAlign: "center",
+                cellClick: function(e, cell) {
+                    alert("XD")
+                }
+            },
+            {
+                title: "Eliminar",
+                field: "IDUSER",
+                formatter: downIcon,
+                hozAlign: "center",
+                cellClick: function(e, cell) {
+                    alert("XD")
+                }
+            },
+        ],
 
 
-});
+    });
 </script>

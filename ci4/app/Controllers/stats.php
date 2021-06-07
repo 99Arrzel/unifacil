ecae8e0037f14929d27869b3fff2d899a73cfd1a
rats4final <80012704+rats4final@users.noreply.github.com>
2021-06-07 03:01:19 -0400

    redireccionando a los "reportes"

---
+++ ci4/app/Controllers/stats.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+class Imagen extends BaseController 
+{
+    public function index()
+    {
+        return view('stats');
+        
+        
+    }
+}
---
+++ ci4/app/Views/stats.php
@@ -0,0 +1,3 @@
+<?php
+header("Location: stats/usage.png");
+?>

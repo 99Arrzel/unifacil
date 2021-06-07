5d046873c9483b10a863a82aa49b276e9ef9d7c0
CraniumAdamantium <af.carrillo@live.com>
2021-06-07 17:30:08 -0400

    XD

--- ci4/app/Views/admin/usuarios.php
+++ ci4/app/Views/admin/usuarios.php
@@ -103,6 +103,7 @@ var table = new Tabulator("#UsuariosActivos", {
         {title:"Email", field:"EMAIL", editor:"input"},
         {title:"Nivel", field:"NIVEL", editor:"select", editorParams:{values:["Admin", "Publisher", "Usuario"]}},
         {title:"Suscrito", field:"SUSCRITO",  formatter:"tickCross", sorter:"boolean", editor:true},
+        {title:"Editar", field:"IDUSER", formatter:"buttonCross", }
     ],
 
 

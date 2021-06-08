0f0422f26530d9c3920a6be18d5d8fa7d40410cd
rats4final <80012704+rats4final@users.noreply.github.com>
2021-06-08 17:51:28 -0400

    empezando

---
+++ ci4/app/Controllers/LibroFiltro.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\ModeloLibroFiltro;
+
+class Libro extends BaseController 
+{
+    public function index()
+    {
+        echo view('templates/header');//navbar
+        //TODO::cambios aqui
+        //$datoslibro = (new ModeloLibroFiltro())->listarlibros();//Envio todo el puto directorio dentro de dir
+        $libro= new ModeloLibroFiltro();
+        $datoslibro = $libro->listarLibros();
+        $mensaje = session('mensaje');
+    
+        $imagen = new ModeloLibroFiltro();
+        $datosimagen = $imagen->listarImagenes();
+        $data = [
+            "libro" => $datoslibro,//datosdir
+            "mensaje"=>$mensaje,
+            "imagen" => $datosimagen
+            //mensaje para la alerta
+        ];
+        return view('abm_libro_filtro',$data);
+
+        
+        /*
+        $libro = new ModeloLibroFiltro();
+        $datos = $libro->listarLibros();
+        $mensaje = session('mensaje');
+        $imagen = new ModeloLibroFiltro();
+        $datosImg = $imagen->listarImagenes();
+        $data = [
+            "datos" => $datos,//datos
+            "datosImg" => $datosImg,
+            "mensaje"=>$mensaje//mensaje para la alerta
+        ];
+
+
+        return view('abm_libro',$data);
+        */
+        
+    }
+
+    public function crearLibro()
+    {
+        //print_r($_POST); se fija si los datos llegan
+        $datos = [
+            "nombreLibro"=>$_POST['nombreLibro'],
+            "year"=>$_POST['year'],
+            "edicion"=>$_POST['edicion'],
+            "dirDoc"=>$_POST['dirDoc'],
+            "tblImagen_idtblImagen"=>$_POST['tblImagen_idtblImagen']
+            //nombre del campo de la base=> nombre o id del input
+        ];
+        $libro = new ModeloLibroFiltro(); // nuevo objeto libro
+        $respuesta = $libro->insertarLibro($datos); //manda los datos al modelo
+        if($respuesta > 0){ 
+            return redirect()->to(base_url().'/libro')->with('mensaje','1');
+        }else {
+            return redirect()->to(base_url().'/libro')->with('mensaje','0');
+        }
+    }
+    public function actualizarLibro()
+    {
+        //TODO: terminar de aniadir los datos
+        print_r($_POST); //se fija si los datos llegan
+        $datos = [
+            "idtblLibro"=>$_POST['idtblLibro'],//el update es lo mismo que el insert pero hay que adjuntar el ID a modificar
+            "nombreLibro"=>$_POST['nombreLibro'],
+            "year"=>$_POST['year'],
+            "edicion"=>$_POST['edicion'],
+            "dirDoc"=>$_POST['dirDoc'],
+            "estado"=>$_POST['estado']
+            //nombre del campo de la base=> nombre o id del input
+        ];
+        $datosimg = [
+            "idtblImagen"=>$_POST['idtblImagen'],
+            "nombreImagen"=>$_POST['nombreImagen']
+        ];
+
+        $idtblLibro=$_POST['idtblLibro'];//id para la consulta
+        $libro = new ModeloLibroFiltro(); // nuevo objeto libro
+        $respuesta = $libro->actualizarLibro($datos,$idtblLibro);
+
+        $idtblImagen=$_POST['idtblImagen'];
+        $imagen = new ModeloLibroFiltro();
+        $respuestaimg = $imagen->actualizarImagen($datosimg,$idtblImagen);
+
+        if($respuesta){//if true
+            return redirect()->to(base_url().'/libro')->with('mensaje','2');
+        }else {
+            return redirect()->to(base_url().'/libro')->with('mensaje','3');
+        }
+    }
+
+    /* me di cuenta que era redundante XD 
+    public function eliminarLibroLogic()
+    {
+        //TODO: terminar de aniadir los datos
+        print_r($_POST); //se fija si los datos llegan
+        $datos = [
+            "idtblLibro"=>$_POST['idtblLibro'],//el update es lo mismo que el insert pero hay que adjuntar el ID a modificar
+            "nombreLibro"=>$_POST['nombreLibro'],
+            "year"=>$_POST['year'],
+            "edicion"=>$_POST['edicion'],
+            "dirDoc"=>$_POST['dirDoc'],
+            "estado"=>'1'
+            //nombre del campo de la base=> nombre o id del input
+        ];
+        
+        $idtblLibro=$_POST['idtblLibro'];//id para la consulta
+        $libro = new ModeloLibroFiltro(); // nuevo objeto libro
+        $respuesta = $libro->eliminarLibroLogic($datos,$idtblLibro);
+
+        if($respuesta){//if true
+            return redirect()->to(base_url().'/libro')->with('mensaje','2');
+        }else {
+            return redirect()->to(base_url().'/libro')->with('mensaje','3');
+        }
+    }
+
+    */
+   
+    public function obtenernombreLibro($idtblLibro,$idtblImagen)
+    {
+        echo view('templates/header');//navbar
+        $data =["idtblLibro" => $idtblLibro];//mismo que en la linea 61
+        $libro = new ModeloLibroFiltro();
+        $respuesta=$libro->obtenernombreLibro($data);
+
+        $dataimg = ["idtblImagen" => $idtblImagen];
+        $imagen = new ModeloLibroFiltro();
+        $respuestaimg=$imagen->obtenerNombreImagen($dataimg);
+
+        $datos=[
+            "datos" => $respuesta,
+            "datosimg" =>$respuestaimg
+        ];
+
+        return view('actualizarLibro',$datos);
+    }
+
+    public function eliminarLibro($idtblLibro)
+    {
+        $libro = new ModeloLibroFiltro();
+        $data = ["idtblLibro" => $idtblLibro];
+
+        $respuesta = $libro->eliminarLibro($data);
+
+        if($respuesta){//if true
+            return redirect()->to(base_url().'/libro')->with('mensaje','4');
+        }else {
+            return redirect()->to(base_url().'/libro')->with('mensaje','5');
+        }
+    }
+}
---
+++ ci4/app/Models/ModeloLibroFiltro.php
@@ -0,0 +1,130 @@
+<?php
+namespace App\Models;
+use CodeIgniter\Model;
+
+class ModeloLibroFiltro extends Model
+{
+    public function listarLibros()
+    {                               
+        $Libros = $this->db->query("SELECT
+        `lib`.`nombreLibro` AS `nombreLibro`,
+        `lib`.`year` AS `year`,
+        `lib`.`edicion` AS `edicion`,
+        `lib`.`dirDoc` AS `dirDoc`,
+        `ima`.`dirImagen` AS `dirImagen`,
+        GROUP_CONCAT(
+            DISTINCT `n`.`nombreAutor` SEPARATOR ', '
+        ) AS `autores`,
+        GROUP_CONCAT(
+            DISTINCT `tag`.`nombreTag` SEPARATOR ', '
+        ) AS `tags`, GROUP_CONCAT( DISTINCT ff.idfiltroFinal SEPARATOR', ') as filtro 
+    FROM
+                       
+                            `altillo`.`tblLibro` `lib`
+                        JOIN `altillo`.`tblAutor_has_tblLibro` `c`
+                        
+                    JOIN `altillo`.`tblAutor` `n`
+                    
+                JOIN `altillo`.`tblImagen` `ima`
+                
+            JOIN `altillo`.`tblTag` `tag`
+       
+        JOIN `altillo`.`tblTag_has_tblLibro` `ltag`
+        JOIN filtroFinal ff JOIN tblfiltroFinal_has_tblLibro flib
+    WHERE
+        (
+            (
+                `lib`.`idtblLibro` = `c`.`tblLibro_idtblLibro`
+            ) AND(
+                `c`.`tblAutor_idtblAutor` = `n`.`idtblAutor`
+            ) AND(
+                `ima`.`idtblImagen` = `lib`.`tblImagen_idtblImagen`
+            ) AND(
+                `lib`.`idtblLibro` = `ltag`.`tblLibro_idtblLibro`
+            ) AND(
+                `tag`.`idtblTag` = `ltag`.`tblTag_idtblTag`
+            )AND ff.idfiltroFinal = flib.idfiltroFinal_Union AND lib.idtblLibro = flib.idtblLibro_Union
+        )
+    GROUP BY
+        `lib`.`nombreLibro`");
+
+        return $Libros->getResult();        
+    }
+
+    //protected $table = 'tblLibro';
+    //protected $primaryKey = 'idtblLibro';
+    //protected $allowedFields = ['idtblLibro','nombreLibro',
+    //'year','edicion','dirDor',
+    //'tblImagen_idtblImagen'];    
+    
+    public function insertarLibro($datos)
+    {
+        $Libros = $this->db->table('tblLibro');//selecciona la tabla
+        $Libros->insert($datos); //inserta los datos
+        $this->mandarMails($datos['nombreLibro']);
+        return $this->db->insertID();//devuelve el ultimo ID
+    }
+
+    public function actualizarLibro($data,$idtblLibro)
+    {
+        $Libros = $this->db->table('tblLibro');
+        $Libros->set($data);//funcion update
+        $Libros->where('idtblLibro',$idtblLibro);
+        return $Libros->update();
+    }
+
+    /*
+    public function eliminarLibroLogic($data,$idtblLibro)
+    {
+        $Libros = $this->db->table('tblLibro');
+        $Libros->set($data);//funcion update
+        $Libros->where('idtblLibro',$idtblLibro);
+        return $Libros->update();
+    }*/
+
+    public function actualizarImagen($data,$idtblImagen)
+    {
+        $Imagenes = $this->db->table('tblImagen');
+        $Imagenes->set($data);//funcion update
+        $Imagenes->where('idtblImagen',$idtblImagen);
+        return $Imagenes->update();
+    }
+
+    public function obtenerNombreLibro($data)
+    {
+        $Libros = $this->db->table('tblLibro');
+        $Libros->where($data);
+        return $Libros->get()->getResultArray(); //array de arrays :v
+    }
+    
+    public function obtenerNombreImagen($data)
+    {
+        $Imagenes = $this->db->table('tblImagen');
+        $Imagenes->where($data);
+        return $Imagenes->get()->getResultArray();
+    }
+
+    public function eliminarLibro($data)
+    {
+        $Libros = $this->db->table('tblLibro');
+        $Libros->where($data);
+        return $Libros->delete();
+    }
+    public function mandarMails(string $libro)
+    {
+        $query = $this->db->query('SELECT us.email as mail
+        FROM tblUsuario as us
+        WHERE us.suscrito = "1"');
+        $miArray = $query->getResultArray();
+        foreach ($miArray as $ar)
+        {
+            mail($ar['mail'], "Nuevo libro añadido: " .$libro, "Se añadió un libro");
+        }
+    }
+    public function listarImagenes()
+    {
+        $query = $this->db->query("SELECT im.idtblImagen as IDimagen, im.nombreImagen
+        FROM tblImagen as im");
+        return $query->getResultArray();
+    }
+}

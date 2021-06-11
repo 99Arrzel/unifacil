682f66253eceb68f7a6476756cc0d1a0f4d6b960
rats4final <80012704+rats4final@users.noreply.github.com>
2021-06-10 23:10:01 -0400

    fdfd

--- ci4/app/Views/abm_autor.php
+++ ci4/app/Views/abm_autor.php
@@ -93,6 +93,8 @@
     <div class="buttonContainer">
         <button class="btn" id="jsPDF" >Export PDF with jsPDF</button>
         <button class="btn" id="browserPrint" >Export PDF with browser print</button>
+        <button class="btn" onclick="demoFromHTML()" >PDF</button>
+
         <p id="styledHeaderLink">See a similar page but with a <a href="styled-header.html">fancy styled header</a>.</p>
       </div>
       <iframe id="txtArea1" style="display:none"></iframe>
@@ -136,6 +138,46 @@
     return (sa);
 }
   
+
+function demoFromHTML() {
+    var pdf = new jsPDF('p', 'pt', 'letter');
+    // source can be HTML-formatted string, or a reference
+    // to an actual DOM element from which the text will be scraped.
+    source = $('#styledTable')[0];
+
+    // we support special element handlers. Register them with jQuery-style 
+    // ID selector for either ID or node name. ("#iAmID", "div", "span" etc.)
+    // There is no support for any other type of selectors 
+    // (class, of compound) at this time.
+    specialElementHandlers = {
+        // element with id of "bypass" - jQuery style selector
+        '#bypassme': function (element, renderer) {
+            // true = "handled elsewhere, bypass text extraction"
+            return true
+        }
+    };
+    margins = {
+        top: 80,
+        bottom: 60,
+        left: 40,
+        width: 522
+    };
+    // all coords and widths are in jsPDF instance's declared units
+    // 'inches' in this case
+    pdf.fromHTML(
+    source, // HTML string or DOM elem ref.
+    margins.left, // x coord
+    margins.top, { // y coord
+        'width': margins.width, // max width of content on PDF
+        'elementHandlers': specialElementHandlers
+    },
+
+    function (dispose) {
+        // dispose: object with X, Y of the last line add to the PDF 
+        //          this allow the insertion of new lines after html
+        pdf.save('Test.pdf');
+    }, margins);
+}
   </script>
 
 <!-- For jsPDF -->

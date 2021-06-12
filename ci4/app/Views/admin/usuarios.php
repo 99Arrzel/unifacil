a2169648e5c4570a6b5fd1c04cf8001618f98525
CraniumAdamantium <64380067+CraniumAdamantium@users.noreply.github.com>
2021-06-12 17:30:48 -0400

    xd

--- ci4/app/Views/admin/usuarios.php
+++ ci4/app/Views/admin/usuarios.php
@@ -105,7 +105,7 @@ endif;
                         <form class='' action='/ListarUsuarios/guardar' method='post'>
                             <tbody>
                                 <tr>
-                                    
+
                                     <td><input id="nombreModal" autocomplete="off" type='text' name='nombre'
                                             class='form-control'></input></td>
                                     <td><input id="apellidoModal" autocomplete="off" type='text' name='apellido'
@@ -135,7 +135,8 @@ endif;
                                             <?php endforeach; ?>
                                         </select></td>
                                     <!-- Nivel-->
-                                    <td><button id="btnModal" name="btnModal" value="" type='submit' class='btn btn-success form-control'>Enviar
+                                    <td><button id="btnModal" name="btnModal" value="" type='submit'
+                                            class='btn btn-success form-control'>Enviar
                                     </td>
                                 </tr>
                             </tbody>
@@ -152,11 +153,38 @@ endif;
 <!-- FIN de modal para insertar usuario -->
 <!-- Insertar arriba -->
 <br>
+<div class='container-fluid'>
+    <h1 class="text-center">Lista de Usuarios</h1>
+    <h3 id="anuncio" class="text-center">De alta</h3>
+    <div class='row'>
+        <div class='col-12'>
+            <div class='table table-bordered bg-dark text-light'>
+                <div class="table-responsive">
+                    <table class="table table-hover" id="tblUsuarios">
+                        <thead class="thead-dark">
+                            <tr>
+                                <th>ID</th>
+                                <th>Nombre</th>
+                                <th>Apellido</th>
+                                <th>Login</th>
+                                <th>Correo</th>
+                                <th>Nivel</th>
+                                <th>Suscrito</th>
+                                <th>Editar</th>
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 <div class="container-fluid">
     <div class="col-lg-12">
         <div class='table table-bordered bg-dark text-light'>
             <div class="table-responsive">
-                <table class="table table-hover" id="tblUsuarios">
+                <table class="table table-hover" id="tblUsuariosBaja">
                     <thead class="thead-dark">
                         <tr>
                             <th>ID</th>
@@ -174,7 +202,84 @@ endif;
         </div>
     </div>
 </div>
+
 <script>
+    //Script para activos
+    $(document).ready(function() {
+        var tabla = $('#tblUsuariosBaja').DataTable({
+            data: <?php echo json_encode($usuarioBaja)?> ,
+            "columnDefs": [{
+                "targets": 7,
+                "data": null,
+                "defaultContent": "<button class='btn btn-warning form-control' data-toggle='modal' data-target='#dropEdit'>Editar</button>"
+            }],
+            columns: [{
+                    data: 'IDUSER',
+                    visible: false,
+                },
+                {
+                    data: 'NOMBRE'
+                },
+                {
+                    data: 'APELLIDO'
+                },
+                {
+                    data: 'LOGIN'
+                },
+                {
+                    data: 'EMAIL'
+                },
+                {
+                    data: 'NIVEL'
+                },
+                {
+                    data: 'SUSCRITO'
+                },
+            ],
+            language: {
+                "decimal": "",
+                "emptyTable": "No hay datos",
+                "info": "Mostrando _START_ a _END_ de _TOTAL_ registros",
+                "infoEmpty": "Mostrando 0 a 0 de 0 registros",
+                "infoFiltered": "(Filtro de _MAX_ total registros)",
+                "infoPostFix": "",
+                "thousands": ",",
+                "lengthMenu": "Mostrar _MENU_ registros",
+                "loadingRecords": "Cargando...",
+                "processing": "Procesando...",
+                "search": "Buscar:",
+                "zeroRecords": "No se encontraron coincidencias",
+                "paginate": {
+                    "first": "Primero",
+                    "last": "Ultimo",
+                    "next": "Siguiente",
+                    "previous": "Anterior"
+                },
+                "aria": {
+                    "sortAscending": ": Activar orden de columna ascendente",
+                    "sortDescending": ": Activar orden de columna desendente"
+                }
+            },
+        });
+        $('#tblUsuarios tbody').on('click', 'button', function() {
+            var data = tabla.row($(this).parents('tr')).data();
+            document.getElementById("nombreModal").value = data['NOMBRE'];
+            document.getElementById("apellidoModal").value = data['APELLIDO'];
+            document.getElementById("loginModal").value = data['LOGIN'];
+            document.getElementById("emailModal").value = data['EMAIL'];
+            document.getElementById("passwordModal").value = data['CONTRASEÑA'];
+            document.getElementById("suscritoModal").value = data['SUSCRITO'];
+            document.getElementById("nivelModal").value = data['NIVELID'];
+            document.getElementById("btnModal").value = data['IDUSER'];
+            //data-toggle="modal" data-target="#staticBackdrop"
+            //alert(data['NOMBRE'] + "id");
+            console.log(data);
+        });
+    });
+</script>
+<!-- ACTIVOS ABAJO -->
+<script>
+    //Script para activos
     $(document).ready(function() {
         var tabla = $('#tblUsuarios').DataTable({
             data: <?php echo json_encode($usuario)?> ,

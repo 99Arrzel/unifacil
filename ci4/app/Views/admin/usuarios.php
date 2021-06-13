02d67bfd7dd3f8c7f31d485aec2b67866fa4e00d
CraniumAdamantium <64380067+CraniumAdamantium@users.noreply.github.com>
2021-06-13 15:42:57 -0400

    ERASE

--- ci4/app/Views/admin/usuarios.php
+++ ci4/app/Views/admin/usuarios.php
@@ -1,13 +1,3 @@
-<?php
-echo $_SERVER['HTTP_REFERER'];
-echo "---------------------------";
-$http_origin = $_SERVER['HTTP_REFERER'];
-
-if ($http_origin == "https://www.proyecto3.tk" || $http_origin == "https://proyecto3.tk")
-{  
-    header("Access-Control-Allow-Origin: $http_origin");
-}
-?>
 <br>
 <div class='container-fluid'>
     <h1 class="text-center">Crear Usuario</h1>

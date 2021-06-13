d3961f80d4501d1a85c290436fefc50e29953b74
CraniumAdamantium <64380067+CraniumAdamantium@users.noreply.github.com>
2021-06-12 20:30:04 -0400

    EraseID

--- ci4/app/Views/admin/usuarios.php
+++ ci4/app/Views/admin/usuarios.php
@@ -328,34 +328,30 @@ endif;
             buttons: [{
                     extend: 'copy',
                     text: 'Copiar',
-                    columns: [1, 2, 4, 5, 6],
+                    columns: [0, 1, 2, 3, 4],
                 },
                 {
                     extend: 'csv',
                     text: 'CSV',
-                    columns: [1, 2, 4, 5, 6],
+                    columns: [0, 1, 2, 3, 4],
                 },
                 {
                     extend: 'excel',
                     text: 'EXCEL',
-                    columns: [1, 2, 4, 5, 6],
+                    columns: [0, 1, 2, 3, 4],
                 },
                 {
                     extend: 'pdf',
                     text: 'PDF',
-                    columns: [1, 2, 4, 5, 6],
+                    columns: [0, 1, 2, 3, 4],
                 },
                 {
                     extend: 'print',
                     text: 'Imprimir',
-                    columns: [1, 2, 4, 5, 6],
+                    columns: [0, 1, 2, 3, 4],
                 },
             ],
             columns: [{
-                    data: 'IDUSER',
-                    visible: false,
-                },
-                {
                     data: 'NOMBRE'
                 },
                 {

1926f0ad151bade41b57e75730f5f03773623526
rats4final <80012704+rats4final@users.noreply.github.com>
2021-06-19 02:34:42 -0400

    finish

--- ci4/app/Views/abm_libro_relacion.php
+++ ci4/app/Views/abm_libro_relacion.php
@@ -115,4 +115,17 @@
                 swal('🤡', 'Error al eliminar', 'error');
             }
         </script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<link href="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/css/select2.min.css" rel="stylesheet" />
+<script src="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/js/select2.min.js"></script>
+
+<script type="text/javascript">
+$(document).ready(function () {
+//change selectboxes to selectize mode to be searchable
+   $("select").select2();
+});
+
+</script>
+
     </body>

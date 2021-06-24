e3a3c4d9f239f0d9ad58c015f60210c0b0992c48
rats4final <80012704+rats4final@users.noreply.github.com>
2021-06-24 18:41:22 -0400

    dark

--- ci4/app/Views/vistas/gestion.php
+++ ci4/app/Views/vistas/gestion.php
@@ -5,7 +5,7 @@
             
             <div class='card-body'>
                 <h5 class='card-title' align='center'><?= $zzz?></h5>
-                <p class='card text-center'>Ver material de <?= $zzz?></p>
+                <p class='card text-center text-dark'>Ver material de <?= $zzz?></p>
                 <br>
                 <p style='text-align:center;'>
                     <a href='<?= "http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'] ."/". $zzz?>' class='btn btn-primary' style='align:center'>Leer</a>

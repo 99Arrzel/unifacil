8490d4db8f678e99f1e3ce01400803a003cf44ac
rats4final <80012704+rats4final@users.noreply.github.com>
2021-06-24 18:46:15 -0400

    success

--- ci4/app/Views/vistas/semestre.php
+++ ci4/app/Views/vistas/semestre.php
@@ -1,7 +1,7 @@
 <div class='container' style='margin-top:3em;'>
     <div class='card-columns custom-columns'>
         <?php foreach ($semestre as $zzz): ?>
-        <div class='card'>
+        <div class='card border-success mb-3'>
             
             <div class='card-body'>
                 <h5 class='card-title' align='center'><?= $zzz?> semestre</h5>
